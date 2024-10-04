bd4d70943f930a01c2843db2a10042286492489a
rishu <rrishu212@gmail.com>
2024-10-04 22:58:05 +0530

    minor update ||

--- backend/util/database.js
+++ backend/util/database.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
     dialect: 'mysql',
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT || 3307
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3307
 });
