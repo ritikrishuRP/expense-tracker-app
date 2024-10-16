a94618c0284bdb848a4aa44ff746c39674106311
rishu <rrishu212@gmail.com>
2024-10-16 20:40:48 +0530

    minor change

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,20 @@ const cors = require('cors');
 const dotenv = require("dotenv");
 const helmet = require('helmet');
 const morgan = require('morgan');
-const fs = require('fs')
+const fs = require('fs');
 
-const userRoute = require('./route/user.route')
+const userRoute = require('./route/user.route');
 const expenseRoute = require('./route/expense.route');
 const purchaseRoute = require('./route/purchase.route');
 const premiumFeaturesRoute = require('./route/premiumFeatures.route');
 const resetPassword = require('./route/forgotpassword.route');
-const reportRoute = require('./route/report.route')
+const reportRoute = require('./route/report.route');
 
 const User = require('./model/user.model');
 const Expense = require('./model/expense.model');
 const Order = require('./model/order.model');
 const ForgotPassword = require('./model/forgotpassword.model');
-const Download = require('./model/download.model')
+const Download = require('./model/download.model');
 
 dotenv.config();
 
@@ -28,20 +28,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(helmet());
-//app.use(express.static('public'));
+app.use(helmet());
+app.use(express.static('public'));
 
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname , 'access.log'),{flags : 'a'})
-
-app.use(morgan('combined',{ stream :accessLogStream}))
+const accessLogStream = fs.createWriteStream(path.join(__dirname , 'access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // Serve static files from the 'frontend' directory
 app.use(express.static(path.join(__dirname, '..', 'frontend'), {
   index: false // This will prevent index.html from being served by default
 }));
 
-
+// Define routes
 app.use('/api', userRoute);
 app.use('/expense', expenseRoute);
 app.use('/purchase', purchaseRoute);
@@ -49,35 +47,44 @@ app.use('/premium', premiumFeaturesRoute);
 app.use('/password', resetPassword);
 app.use('/report', reportRoute);
 
-// app.get('/index.html', (req, res) => {
-//   res.sendFile(path.join(__dirname,'..','frontend','index.html'));
-// });
-
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend', 'signup.html'));
 });
 
-app.get('/login', (req,res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'login.html'));
-})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'login.html'));
+});
 
-//console.log(process.env.DB_HOST);
-
+// Define relationships
 User.hasMany(Expense, { foreignKey: 'userId' });
 Expense.belongsTo(User, { foreignKey: 'userId' });
 
 User.hasMany(Order, { foreignKey: 'userId' });
-Order.belongsTo(User, { foreignKey: 'userId' })
+Order.belongsTo(User, { foreignKey: 'userId' });
 
 User.hasMany(ForgotPassword, { foreignKey: 'userId' });
 ForgotPassword.belongsTo(User, { foreignKey: 'userId' });
 
-User.hasMany(Download, { foreignKey: 'userId' })
-Download.belongsTo(User, { foreignKey: 'userId' })
+User.hasMany(Download, { foreignKey: 'userId' });
+Download.belongsTo(User, { foreignKey: 'userId' });
 
+// Global error handling
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+// Sync database and start server
 sequelize
   .sync()
   .then(result => {
-    app.listen(3000);
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.error('Database connection error:', err);
+  });
--- backend/util/database.js
+++ backend/util/database.js
@@ -5,9 +5,10 @@ const Sequelize = require('sequelize');
 // });
 // module.exports = sequelize;
 
-const sequelize = new Sequelize(_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
     dialect: 'mysql',
-    host: process.env.DB_HOST
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3307
 });
 
 module.exports = sequelize;
